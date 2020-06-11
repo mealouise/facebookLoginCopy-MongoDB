21b4588c14e2b0376c1b17ca99c46e0c4096d46c
mealouise <esheridan45@gmail.com>
2020-06-11 21:06:35 +0100

    changed port for azure deployment

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.set('view engine', '.hbs');
 
 app.use('/', userRouter);
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("you are listening on port 3000")
 })
